882fc5c1ec3d533286710bf861a4b98b49d88e4d
Christopher-Java <chrlom0905@skola.goteborg.se>
2023-11-13 12:49:40 +0100

    Assignment 2

---
+++ src/com/yrgo/services/calls/CallHandlingServiceImpl.java
@@ -0,0 +1,33 @@
+package com.yrgo.services.calls;
+
+import com.yrgo.domain.Action;
+import com.yrgo.domain.Call;
+import com.yrgo.services.customers.CustomerManagementMockImpl;
+import com.yrgo.services.customers.CustomerManagementService;
+import com.yrgo.services.customers.CustomerNotFoundException;
+import com.yrgo.services.diary.DiaryManagementService;
+import com.yrgo.services.diary.DiaryManagementServiceMockImpl;
+
+import java.util.Collection;
+
+public class CallHandlingServiceImpl implements CallHandlingService {
+
+    CustomerManagementService customerManagementService;
+    DiaryManagementService diaryManagementService;
+
+    public CallHandlingServiceImpl(CustomerManagementService customerManagementService, DiaryManagementService diaryManagementService) {
+        this.customerManagementService = customerManagementService;
+        this.diaryManagementService = diaryManagementService;
+    }
+
+    @Override
+    public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException {
+        customerManagementService.recordCall(customerId, newCall);
+
+        for (Action action : actions) {
+            diaryManagementService.recordAction(action);
+        }
+    }
+
+
+}
