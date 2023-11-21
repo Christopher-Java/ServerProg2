045487a7bbab65da9eefbd825c0de39b49b3f42d
Christopher-Java <chrlom0905@skola.goteborg.se>
2023-11-21 13:38:50 +0100

    Assignment4

---
+++ src/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,32 @@
+package com.yrgo.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformanceTimingAdvice {
+
+    public Object performTimingMeasurement(ProceedingJoinPoint method) throws Throwable {
+
+        //before
+        long startTime = System.nanoTime();
+
+        try {
+            //proceed to target
+            Object value = method.proceed();
+            return value;
+        } finally {
+            //after
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            System.out.println("The method " + method.getSignature().getName() + " took " + timeTaken / 1000000);
+        }
+    }
+
+   /*
+    //before Advice
+    public void beforeAdviceTesting() {
+        System.out.println("Now entering method ....");
+    }
+    */
+}
